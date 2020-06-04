4b82c70842b260ed637dd397c5db38bdd8c30509
Troy Marker <developer@phoenixhospman.com>
2020-06-04 11:05:38 -0500

    06/04/2020 11:05 Commit
    Added Launcher icons

--- app/src/main/java/com/phoenixhosman/phoenixdbmanagers/ActivityMain.java
+++ app/src/main/java/com/phoenixhosman/phoenixdbmanagers/ActivityMain.java
@@ -3,12 +3,17 @@ package com.phoenixhosman.phoenixdbmanagers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
